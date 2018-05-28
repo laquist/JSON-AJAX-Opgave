a7d9b85ca86f8da6362a7d3c9644fbc63d1c4a2b
Lars Erling Quist Lynnerup <lars9865@edu.campusvejle.dk>
2018-05-28 10:56:34 +0200

    First upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// JSON Link: https://learnwebcode.github.io/json-example/animals-1.json
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
